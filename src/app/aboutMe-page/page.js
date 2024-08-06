9ad0da1c2cc997bd0fc09e6f18e0adb31e231e9c
Washington <washington.contato1327@gmail.com>
2024-08-06 13:49:48 -0300

    Ajustando tamanho de fontes e adicionando projeto

--- src/app/aboutMe-page/page.js
+++ src/app/aboutMe-page/page.js
@@ -1,102 +1,136 @@
-'use client';
+"use client";
 
-import { useState } from 'react';
-import { Box, Button, Pagination, Stack, Typography } from '@mui/material';
-import Link from 'next/link';
-import Styles from './aboutme.module.css';
-import Header from '../pages/header/header';
+import { useState } from "react";
+import { Box, Button, Pagination, Stack, Typography } from "@mui/material";
+import Link from "next/link";
+import Styles from "./aboutme.module.css";
+import Header from "../pages/header/header";
 
 const AboutMePage = () => {
-    const [page, setPage] = useState(1);
+  const [page, setPage] = useState(1);
 
-    const handlePageChange = (event, value) => {
-        setPage(value);
-    };
+  const handlePageChange = (event, value) => {
+    setPage(value);
+  };
 
-    const renderContent = () => {
-        switch (page) {
-            case 1:
-                return (
-                    <>
-                        <Typography variant='h2' className={Styles.h2}>Sobre Mim</Typography>
-                        <Typography className={Styles.p} variant='body1'>
-                            Sou um programador front-end formado em Análise e Desenvolvimento de Sistemas e atualmente cursando Engenharia de Software. Natural de Pernambuco, atualmente divido meu tempo entre Recife e São Paulo, cidade pela qual me apaixonei desde a primeira visita.
-                            Minha jornada na programação começou na metade de 2021. No início, enfrentei algumas dificuldades, mas com persistência e dedicação, descobri minha capacidade e paixão pela área. A programação representou uma virada de página na minha vida, proporcionando crescimento pessoal e profissional.
-                            Meu objetivo é alcançar meu tão sonhado sucesso, pois sei que tenho a capacidade de sempre evoluir e me aprimorar. O front-end é apenas o começo de uma jornada de sucesso, e estou determinado a continuar avançando e conquistando meu espaço no mundo da tecnologia.
-                        </Typography>
-                    </>
-                );
-            case 2:
-                return (
-                    <>
-                        <Typography variant='h2' className={Styles.h2}>Gostos Pessoais</Typography>
-                        <Typography className={Styles.p} variant='body1'>
-                            Sou uma pessoa apaixonada por viajar e explorar novos lugares. Entre os diversos destinos que já tive a oportunidade de conhecer pelo Brasil, a Chapada dos Veadeiros se destacou como o mais marcante. Lá, tive a chance de me conectar profundamente com a natureza através de suas cachoeiras deslumbrantes e trilhas revitalizantes. A natureza exerce um efeito renovador em mim, e sempre busco estar em contato com ela.
-                            Além das viagens, tenho um grande apreço por esportes como futebol e ciclismo, que me ajudam a manter um estilo de vida ativo e saudável. No universo dos games, sou fã do estilo battlegrounds, que sempre me proporciona momentos de diversão e desafio.
-                            Valorizo muito o tempo com minha família e adoro aproveitar os fins de semana para estar com eles, seja em atividades ao ar livre ou em momentos de lazer. Sou um grande apreciador de filmes e séries, e meus gêneros preferidos são os de guerra e de heróis. As histórias intensas e inspiradoras desses gêneros sempre me envolvem e me fascinam.
-                        </Typography>
-                    </>
-                );
-            case 3:
-                return (
-                    <>
-                        <Typography variant='h2' className={Styles.h2}>Motivações</Typography>
-                        <Typography className={Styles.p} variant='body1'>
-                            Minhas principais motivações na vida giram em torno de construir um futuro melhor e viver de maneira plena. Acredito que a tecnologia será a chave para alcançar esses objetivos, pois ela abre portas para inúmeras oportunidades e inovações. Tenho muitos sonhos, entre eles conhecer Machu Picchu, um destino que sempre me fascinou. Estou aguardando ansiosamente pela minha primeira grande oportunidade na área de tecnologia, onde poderei mostrar todo meu potencial e provar que sou capaz. Nunca desistir dos meus sonhos é a força motriz que me impulsiona diariamente, e essa determinação é a minha principal motivação.
-                        </Typography>
-                    </>
-                );
-            default:
-                return null;
-        }
-    };
+  const renderContent = () => {
+    switch (page) {
+      case 1:
+        return (
+          <>
+            <Typography variant="h2" className={Styles.h2}>
+              Sobre Mim
+            </Typography>
+            <Typography className={Styles.p} variant="body1">
+              Sou um programador front-end formado em Análise e Desenvolvimento
+              de Sistemas e atualmente cursando Engenharia de Software. Natural
+              de Pernambuco, atualmente divido meu tempo entre Recife e São
+              Paulo, cidade pela qual me apaixonei desde a primeira visita.
+              Minha jornada na programação começou na metade de 2021. No início,
+              enfrentei algumas dificuldades, mas com persistência e dedicação,
+              descobri minha capacidade e paixão pela área. A programação
+              representou uma virada de página na minha vida, proporcionando
+              crescimento pessoal e profissional. Meu objetivo é alcançar meu
+              tão sonhado sucesso, pois sei que tenho a capacidade de sempre
+              evoluir e me aprimorar. O front-end é apenas o começo de uma
+              jornada de sucesso, e estou determinado a continuar avançando e
+              conquistando meu espaço no mundo da tecnologia.
+            </Typography>
+          </>
+        );
+      case 2:
+        return (
+          <>
+            <Typography variant="h2" className={Styles.h2}>
+              Gostos Pessoais
+            </Typography>
+            <Typography className={Styles.p} variant="body1">
+              Sou uma pessoa apaixonada por viajar e explorar novos lugares.
+              Entre os diversos destinos que já tive a oportunidade de conhecer
+              pelo Brasil, a Chapada dos Veadeiros se destacou como o mais
+              marcante. Lá, tive a chance de me conectar profundamente com a
+              natureza através de suas cachoeiras deslumbrantes e trilhas
+              revitalizantes. A natureza exerce um efeito renovador em mim, e
+              sempre busco estar em contato com ela. Além das viagens, tenho um
+              grande apreço por esportes como futebol e ciclismo, que me ajudam
+              a manter um estilo de vida ativo e saudável. No universo dos
+              games, sou fã do estilo battlegrounds, que sempre me proporciona
+              momentos de diversão e desafio. Valorizo muito o tempo com minha
+              família e adoro aproveitar os fins de semana para estar com eles,
+              seja em atividades ao ar livre ou em momentos de lazer. Sou um
+              grande apreciador de filmes e séries, e meus gêneros preferidos
+              são os de guerra e de heróis. As histórias intensas e inspiradoras
+              desses gêneros sempre me envolvem e me fascinam.
+            </Typography>
+          </>
+        );
+      case 3:
+        return (
+          <>
+            <Typography variant="h2" className={Styles.h2}>
+              Motivações
+            </Typography>
+            <Typography className={Styles.p} variant="body1">
+              Minhas principais motivações na vida giram em torno de construir
+              um futuro melhor e viver de maneira plena. Acredito que a
+              tecnologia será a chave para alcançar esses objetivos, pois ela
+              abre portas para inúmeras oportunidades e inovações. Tenho muitos
+              sonhos, entre eles conhecer Machu Picchu, um destino que sempre me
+              fascinou. Estou aguardando ansiosamente pela minha primeira grande
+              oportunidade na área de tecnologia, onde poderei mostrar todo meu
+              potencial e provar que sou capaz. Nunca desistir dos meus sonhos é
+              a força motriz que me impulsiona diariamente, e essa determinação
+              é a minha principal motivação.
+            </Typography>
+          </>
+        );
+      default:
+        return null;
+    }
+  };
 
-    return (
-        <section className={Styles.containerAbout}>
-            <Box className={Styles.menu}>
-                <Link href="/" passHref>
-                    <Button className={Styles.button} variant="contained" >
-                        Home
-                    </Button>
-                </Link>
-                <Link href="/contact-page" passHref>
-                    <Button className={Styles.button} variant="contained" >
-                        Contacts
-                    </Button>
-                </Link>
-            </Box>
-            <Box className={Styles.containerDescription}>
-                {renderContent()}
-            </Box>
-            <Box className={Styles.containerPagination}>
-                <Stack className={Styles.stack} spacing={2}>
-                    <Pagination
-                        className={Styles.pagination}
-                        count={3}
-                        page={page}
-                        onChange={handlePageChange}
-                        variant="outlined"
-                        shape="rounded"
-                        sx={{
-                            '& .MuiPaginationItem-root': {
-                                backgroundColor: '#e53939',
-                                color: '#fff',
-                                '&:hover': {
-                                    backgroundColor: '#e539398b',
-                                },
-                                '&.Mui-selected': {
-                                    backgroundColor: '#e94226',
-                                    color: '#fff',
-                                },
-                            },
-                        }}
-                    />
-                </Stack>
-            </Box>
-        </section>
-    );
+  return (
+    <section className={Styles.containerAbout}>
+      <Box className={Styles.menu}>
+        <Link href="/" passHref>
+          <Button className={Styles.button} variant="contained">
+            Home
+          </Button>
+        </Link>
+        <Link href="/contact-page" passHref>
+          <Button className={Styles.button} variant="contained">
+            Contacts
+          </Button>
+        </Link>
+      </Box>
+      <Box className={Styles.containerDescription}>{renderContent()}</Box>
+      <Box className={Styles.containerPagination}>
+        <Stack className={Styles.stack} spacing={2}>
+          <Pagination
+            className={Styles.pagination}
+            count={3}
+            page={page}
+            onChange={handlePageChange}
+            variant="outlined"
+            shape="rounded"
+            sx={{
+              "& .MuiPaginationItem-root": {
+                backgroundColor: "#e53939",
+                color: "#fff",
+                "&:hover": {
+                  backgroundColor: "#e539398b",
+                },
+                "&.Mui-selected": {
+                  backgroundColor: "#e94226",
+                  color: "#fff",
+                },
+              },
+            }}
+          />
+        </Stack>
+      </Box>
+    </section>
+  );
 };
 
 export default AboutMePage;
-
-
--- src/app/data/dataBase.js
+++ src/app/data/dataBase.js
@@ -7,9 +7,7 @@ export const dataBase = [
     modalTitle: "CADASTRO FORNECEDOR/PRODUTO",
     modalDescription:
       "Este projeto consiste na construção de um formulário para o cadastro de fornecedores e produtos.",
-    buttonText2: "Repositório",
     buttonLink2: "https://github.com/lemosdiego/vflows-teste.git",
-    buttonText3: "Deploy",
     buttonLink3: "https://vflows-teste.vercel.app/",
   },
   {
@@ -20,9 +18,7 @@ export const dataBase = [
     modalTitle: "Sistema de Pagamento",
     modalDescription:
       "O projeto consiste em um sistema de pagamentos desenvolvido com foco em usabilidade e experiência do usuário. Ele foi projetado para ser intuitivo e eficiente, permitindo que os clientes escolham e realizem pagamentos de maneira simples e rápida. Tecnologias: Next.JS Material UI, CSS.",
-    buttonText2: "Repositório",
     buttonLink2: "https://github.com/lemosdiego/DesafioFront-End-Woovi.git",
-    buttonText3: "Deploy",
     buttonLink3: "https://desafio-front-end-woovi-kappa.vercel.app/",
   },
   {
@@ -33,10 +29,7 @@ export const dataBase = [
     modalTitle: "Portfolio pessoal",
     modalDescription:
       "Este portfólio pessoal foi desenvolvido com Next.js, CSS e media queries para garantir uma experiência de usuário fluida e responsiva em diferentes dispositivos. Utilizando a versão 14.2.5 do Next.js, o projeto exibe uma estrutura modular e bem organizada, com uma clara divisão entre componentes, dados e páginas.",
-    buttonText2: "Repositório",
-    buttonText2: "Repositório",
     buttonLink2: "https://github.com/lemosdiego/portfolio.git",
-    buttonText3: "Deploy",
     buttonLink3: "https://portfolioatalizado.vercel.app/",
   },
   {
@@ -47,11 +40,7 @@ export const dataBase = [
     modalTitle: "Calculadora IMC",
     modalDescription:
       "A Calculadora IMC é uma aplicação web simples que permite aos usuários calcular seu Índice de Massa Corporal IMC com base em sua altura e peso. Utilizando HTML, CSS e JavaScript, a aplicação fornece uma interface amigável onde os usuários podem inserir seus dados, obter o valor do IMC e ver uma classificação sobre seu estado de saúde.",
-    buttonText1: "Ver Mais",
-    buttonLink1: "/vermais/card2",
-    buttonText2: "Repositório",
     buttonLink2: "https://github.com/lemosdiego/IMC.git",
-    buttonText3: "Deploy",
     buttonLink3: "https://imc-delta-seven.vercel.app/",
   },
   {
@@ -62,11 +51,7 @@ export const dataBase = [
     modalTitle: "Lista de tarefas",
     modalDescription:
       "A Lista de Tarefas é uma aplicação web simples e funcional que permite aos usuários criar, gerenciar e rastrear suas tarefas diárias. Utilizando HTML, CSS e JavaScript, essa aplicação proporciona uma interface intuitiva para adicionar novas tarefas, marcar tarefas como concluídas e remover tarefas da lista.",
-    buttonText1: "Ver Mais",
-    buttonLink1: "/vermais/card2",
-    buttonText2: "Repositório",
     buttonLink2: "https://exemplo.com/visitar/card3",
-    buttonText3: "Deploy",
     buttonLink3: "https://tarefas-diarias-omega.vercel.app/",
   },
   {
@@ -77,11 +62,7 @@ export const dataBase = [
     modalTitle: "Quizz",
     modalDescription:
       "O Quizz com Angular é uma aplicação web divertida e educativa, construída utilizando o framework Angular. Este projeto permite aos usuários participar de quizzes interativos em uma variedade de tópicos, responder perguntas, receber feedback imediato e ver suas pontuações finais.",
-    buttonText1: "Ver Mais",
-    buttonLink1: "/vermais/card2",
-    buttonText2: "Repositório",
     buttonLink2: "https://github.com/lemosdiego/quizz_angular.git",
-    buttonText3: "Deploy",
     buttonLink3: "https://quizz-angular-nine.vercel.app/",
   },
   {
@@ -92,11 +73,7 @@ export const dataBase = [
     modalTitle: "Previsão do tempo",
     modalDescription:
       "Esta aplicação de previsão do tempo foi desenvolvida usando HTML, CSS e JavaScript para oferecer informações meteorológicas precisas e atualizadas aos usuários. A aplicação permite que os usuários verifiquem as condições climáticas em tempo real de qualquer cidade ao redor do mundo.",
-    buttonText1: "Ver Mais",
-    buttonLink1: "/vermais/card2",
-    buttonText2: "Repositório",
     buttonLink2: "https://github.com/lemosdiego/previsao-do-tempo.git",
-    buttonText3: "Deploy",
     buttonLink3: "https://previsao-do-tempo-mu.vercel.app/",
   },
   {
@@ -107,11 +84,7 @@ export const dataBase = [
     modalTitle: "Calculadora",
     modalDescription:
       "Projeto simples de uma calculadora utilizando HTML, CSS e JavaScript.",
-    buttonText1: "Ver Mais",
-    buttonLink1: "/vermais/card2",
-    buttonText2: "Repositório",
     buttonLink2: "https://exemplo.com/visitar/card3",
-    buttonText3: "Deploy",
     buttonLink3: "https://calculadora-liard-ten.vercel.app/",
   },
   {
@@ -122,11 +95,18 @@ export const dataBase = [
     modalTitle: "Calculadora",
     modalDescription:
       "Este projeto é um site informativo sobre a história do Android, desenvolvido com HTML e CSS. Ele apresenta a origem, evolução e impacto do Android no mercado de dispositivos móveis, com um design simples e responsivo.",
-    buttonText1: "Ver Mais",
-    buttonLink1: "/vermais/card2",
-    buttonText2: "Repositório",
     buttonLink2: "https://github.com/lemosdiego/projeto-android.git",
-    buttonText3: "Deploy",
     buttonLink3: "https://projeto-android-lime.vercel.app/",
+  },
+  {
+    id: 10,
+    title: "Portfolio",
+    description: "Tecnologias: HTML, CSS e JavaScript",
+    image: "/projetos/projectportfolio.png",
+    modalTitle: "Calculadora",
+    modalDescription:
+      "Primeiro projeto de portfolio feito com HTML CSS e JavaScript.",
+    buttonLink2: "https://github.com/lemosdiego/meu-Portfolio.git",
+    buttonLink3: "https://meu-portfolio-jet-eta.vercel.app/",
   },
 ];
