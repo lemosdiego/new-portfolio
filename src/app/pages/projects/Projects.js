5c0704a901221ac11472778d721255c14383b80f
Washington <washington.contato1327@gmail.com>
2024-08-13 21:06:16 -0300

    Reajustando a seção de projetos

--- src/app/pages/projects/Projects.js
+++ src/app/pages/projects/Projects.js
@@ -1,9 +1,33 @@
+"use client";
+
+import React, { useRef } from "react";
 import ImgMediaCard from "@/app/components/card/Card";
 import { dataBase } from "@/app/data/dataBase";
-const { Grid, Typography, Box } = require("@mui/material");
+import { Grid, Typography, Box } from "@mui/material";
+import Image from "next/image";
 import Styles from "./project.module.css";
 
 const Projects = () => {
+  const containerRef = useRef(null);
+
+  const scrollLeft = () => {
+    if (containerRef.current) {
+      containerRef.current.scrollBy({
+        left: -containerRef.current.offsetWidth / 2,
+        behavior: "smooth",
+      });
+    }
+  };
+
+  const scrollRight = () => {
+    if (containerRef.current) {
+      containerRef.current.scrollBy({
+        left: containerRef.current.offsetWidth / 2,
+        behavior: "smooth",
+      });
+    }
+  };
+
   return (
     <Grid id="projects" className={Styles.grid_container}>
       <Box className={Styles.containerTitle}>
@@ -11,10 +35,28 @@ const Projects = () => {
           Projects
         </Typography>
       </Box>
-      <Box className={Styles.containerProjects}>
+      <Box className={Styles.IconLeft} onClick={scrollLeft}>
+        <Image
+          className={Styles.img}
+          src="/esquerda.svg"
+          alt="Ícone de rolagem para a esquerda"
+          width={100}
+          height={100}
+        />
+      </Box>
+      <Box ref={containerRef} className={Styles.containerProjects}>
         {dataBase.map((card) => (
           <ImgMediaCard key={card.id} cardData={card} />
         ))}
+      </Box>
+      <Box className={Styles.IconRight} onClick={scrollRight}>
+        <Image
+          className={Styles.img}
+          src="/direita.svg"
+          alt="Ícone de rolagem para a direita"
+          width={100}
+          height={100}
+        />
       </Box>
     </Grid>
   );
