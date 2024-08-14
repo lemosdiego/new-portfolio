947846b6a297dba69723f155e78167e369f252d7
Washington <washington.contato1327@gmail.com>
2024-08-14 17:40:08 -0300

    Adicionando timeline

--- src/app/aboutMe-page/page.js
+++ src/app/aboutMe-page/page.js
@@ -1,136 +1,111 @@
-"use client";
-
-import { useState } from "react";
-import { Box, Button, Pagination, Stack, Typography } from "@mui/material";
-import Link from "next/link";
 import Styles from "./aboutme.module.css";
-import Header from "../pages/header/header";
 
-const AboutMePage = () => {
-  const [page, setPage] = useState(1);
-
-  const handlePageChange = (event, value) => {
-    setPage(value);
-  };
-
-  const renderContent = () => {
-    switch (page) {
-      case 1:
-        return (
-          <>
-            <Typography variant="h2" className={Styles.h2}>
-              Sobre Mim
-            </Typography>
-            <Typography className={Styles.p} variant="body1">
-              Sou um programador front-end formado em Análise e Desenvolvimento
-              de Sistemas e atualmente cursando Engenharia de Software. Natural
-              de Pernambuco, atualmente divido meu tempo entre Recife e São
-              Paulo, cidade pela qual me apaixonei desde a primeira visita.
-              Minha jornada na programação começou na metade de 2021. No início,
-              enfrentei algumas dificuldades, mas com persistência e dedicação,
-              descobri minha capacidade e paixão pela área. A programação
-              representou uma virada de página na minha vida, proporcionando
-              crescimento pessoal e profissional. Meu objetivo é alcançar meu
-              tão sonhado sucesso, pois sei que tenho a capacidade de sempre
-              evoluir e me aprimorar. O front-end é apenas o começo de uma
-              jornada de sucesso, e estou determinado a continuar avançando e
-              conquistando meu espaço no mundo da tecnologia.
-            </Typography>
-          </>
-        );
-      case 2:
-        return (
-          <>
-            <Typography variant="h2" className={Styles.h2}>
-              Gostos Pessoais
-            </Typography>
-            <Typography className={Styles.p} variant="body1">
+const About = () => {
+  return (
+    <section className={Styles.about}>
+      <div className={Styles.timeline}>
+        <div className={`${Styles.container} ${Styles.leftContainer}`}>
+          <div className={Styles.iconContainer}></div>
+          <div className={Styles.textBox}>
+            <h2 id="sobre">Sobre mim</h2>
+            <p>
+              Olá, sou Washington Lemos. Sou apaixonado por tecnologia e
+              desenvolvimento de software, com uma forte dedicação em evoluir e
+              crescer na área de programação. Atualmente, estou cursando
+              Engenharia de Software e trabalhando como desenvolvedor front-end,
+              com experiência em projetos pessoais e conhecimento em tecnologias
+              como HTML, CSS, JavaScript, React e Next.js. Meu objetivo de
+              carreira é consolidar minha posição na área de tecnologia e
+              contribuir para projetos inovadores que façam a diferença. Estou
+              sempre em busca de novos desafios e oportunidades para aprender e
+              me aprimorar.
+            </p>
+            <span className={Styles.leftContainerArrow}></span>
+          </div>
+        </div>
+        <div className={`${Styles.container} ${Styles.rightContainer}`}>
+          <div className={Styles.iconContainer}></div>
+          <div className={Styles.textBox}>
+            <h2 id="objetivos">Objetivos de Carreira</h2>
+            <p>
+              Minha meta é me destacar na área de desenvolvimento front-end,
+              contribuindo para projetos de impacto e alcançando reconhecimento
+              profissional. Busco oportunidades que me permitam aplicar meus
+              conhecimentos, aprender novas tecnologias e colaborar com equipes
+              criativas e inovadoras.
+            </p>
+            <span className={Styles.rightContainerArrow}></span>
+          </div>
+        </div>
+        <div className={`${Styles.container} ${Styles.leftContainer}`}>
+          <div className={Styles.iconContainer}></div>
+          <div className={Styles.textBox}>
+            <h2 id="interesses">Áreas de Interesse na Programação</h2>
+            <p>
+              Tenho interesse em desenvolvimento front-end e estou expandindo
+              meus conhecimentos em engenharia de software, desenvolvimento full
+              stack e banco de dados. Acredito que entender tanto o front-end
+              quanto o back-end é crucial para criar aplicações completas e
+              funcionais. Estou sempre em busca de novos desafios para aplicar e
+              aprimorar essas habilidades.
+            </p>
+            <span className={Styles.leftContainerArrow}></span>
+          </div>
+        </div>
+        <div className={`${Styles.container} ${Styles.rightContainer}`}>
+          <div className={Styles.iconContainer}></div>
+          <div className={Styles.textBox}>
+            <h2 id="gostos">Gostos Pessoais</h2>
+            <p>
               Sou uma pessoa apaixonada por viajar e explorar novos lugares.
               Entre os diversos destinos que já tive a oportunidade de conhecer
               pelo Brasil, a Chapada dos Veadeiros se destacou como o mais
               marcante. Lá, tive a chance de me conectar profundamente com a
               natureza através de suas cachoeiras deslumbrantes e trilhas
               revitalizantes. A natureza exerce um efeito renovador em mim, e
-              sempre busco estar em contato com ela. Além das viagens, tenho um
-              grande apreço por esportes como futebol e ciclismo, que me ajudam
-              a manter um estilo de vida ativo e saudável. No universo dos
-              games, sou fã do estilo battlegrounds, que sempre me proporciona
-              momentos de diversão e desafio. Valorizo muito o tempo com minha
-              família e adoro aproveitar os fins de semana para estar com eles,
-              seja em atividades ao ar livre ou em momentos de lazer. Sou um
-              grande apreciador de filmes e séries, e meus gêneros preferidos
-              são os de guerra e de heróis. As histórias intensas e inspiradoras
-              desses gêneros sempre me envolvem e me fascinam.
-            </Typography>
-          </>
-        );
-      case 3:
-        return (
-          <>
-            <Typography variant="h2" className={Styles.h2}>
-              Motivações
-            </Typography>
-            <Typography className={Styles.p} variant="body1">
-              Minhas principais motivações na vida giram em torno de construir
-              um futuro melhor e viver de maneira plena. Acredito que a
-              tecnologia será a chave para alcançar esses objetivos, pois ela
-              abre portas para inúmeras oportunidades e inovações. Tenho muitos
-              sonhos, entre eles conhecer Machu Picchu, um destino que sempre me
-              fascinou. Estou aguardando ansiosamente pela minha primeira grande
-              oportunidade na área de tecnologia, onde poderei mostrar todo meu
-              potencial e provar que sou capaz. Nunca desistir dos meus sonhos é
-              a força motriz que me impulsiona diariamente, e essa determinação
-              é a minha principal motivação.
-            </Typography>
-          </>
-        );
-      default:
-        return null;
-    }
-  };
-
-  return (
-    <section className={Styles.containerAbout}>
-      <Box className={Styles.menu}>
-        <Link href="/" passHref>
-          <Button className={Styles.button} variant="contained">
-            Home
-          </Button>
-        </Link>
-        <Link href="/contact-page" passHref>
-          <Button className={Styles.button} variant="contained">
-            Contacts
-          </Button>
-        </Link>
-      </Box>
-      <Box className={Styles.containerDescription}>{renderContent()}</Box>
-      <Box className={Styles.containerPagination}>
-        <Stack className={Styles.stack} spacing={2}>
-          <Pagination
-            className={Styles.pagination}
-            count={3}
-            page={page}
-            onChange={handlePageChange}
-            variant="outlined"
-            shape="rounded"
-            sx={{
-              "& .MuiPaginationItem-root": {
-                backgroundColor: "#e53939",
-                color: "#fff",
-                "&:hover": {
-                  backgroundColor: "#e539398b",
-                },
-                "&.Mui-selected": {
-                  backgroundColor: "#e94226",
-                  color: "#fff",
-                },
-              },
-            }}
-          />
-        </Stack>
-      </Box>
+              sempre busco estar em contato com ela.
+            </p>
+            <p>
+              Além das viagens, tenho um grande apreço por esportes como futebol
+              e ciclismo, que me ajudam a manter um estilo de vida ativo e
+              saudável. No universo dos games, sou fã do estilo battlegrounds,
+              que sempre me proporciona momentos de diversão e desafio. Valorizo
+              muito o tempo com minha família e adoro aproveitar os fins de
+              semana para estar com eles, seja em atividades ao ar livre ou em
+              momentos de lazer.
+            </p>
+            <p>
+              Sou um grande apreciador de filmes e séries, e meus gêneros
+              preferidos são os de guerra e de heróis. As histórias intensas e
+              inspiradoras desses gêneros sempre me envolvem e me fascinam.
+            </p>
+            <span className={Styles.rightContainerArrow}></span>
+          </div>
+        </div>
+        <div className={`${Styles.container} ${Styles.leftContainer}`}>
+          <div className={Styles.iconContainer}></div>
+          <div className={Styles.textBox}>
+            <h2 id="motivacoes">Motivações</h2>
+            <p>
+              Minha principal motivação para participar da comunidade Código
+              Certo Coders é a oportunidade de crescer profissionalmente e me
+              conectar com outros desenvolvedores apaixonados por tecnologia.
+              Acredito que a comunidade oferece um ambiente enriquecedor para
+              compartilhar conhecimentos, aprender novas habilidades e colaborar
+              em projetos desafiadores. Quero me envolver ativamente, contribuir
+              com minhas habilidades e, ao mesmo tempo, absorver experiências e
+              insights de outros membros. Estou determinado a utilizar essa
+              plataforma para acelerar minha evolução na área de desenvolvimento
+              front-end, alcançar meus objetivos de carreira e mostrar meu
+              potencial em um espaço que valoriza a inovação e o aprendizado
+              contínuo.
+            </p>
+            <span className={Styles.leftContainerArrow}></span>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
 
-export default AboutMePage;
+export default About;
