04cb186565bafc29a88c84f0363dd9fc50f52434
Washington <washington.contato1327@gmail.com>
2024-08-15 13:19:04 -0300

    Ajustes na seção de inicio

--- src/app/pages/home/home.js
+++ src/app/pages/home/home.js
@@ -30,9 +30,9 @@ const Home = () => {
             <Typography className={Styles.p}>
               Sou programador front-end, natural de Recife, mas atualmente
               resido entre Recife e São Paulo. Tenho experiência com HTML, CSS,
-              JavaScript e frameworks como React e Angular. Estou em busca de
-              novas oportunidades para aplicar minhas habilidades e contribuir
-              para projetos inovadores.
+              JavaScript e framework como Next.js. Estou em busca de novas
+              oportunidades para aplicar minhas habilidades e contribuir para
+              projetos inovadores.
             </Typography>
           </Box>
           <Box className={Styles.networks}>
