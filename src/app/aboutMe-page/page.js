f632330c34e88c74385074be6e80e535b70b5856
Washington <washington.contato1327@gmail.com>
2024-08-15 11:36:37 -0300

    Atualizando a páginasobre mim, contando um pouco da minha história através de uma timeline

--- src/app/aboutMe-page/page.js
+++ src/app/aboutMe-page/page.js
@@ -22,16 +22,20 @@ const About = () => {
           <div className={Styles.textBox}>
             <h2 id="sobre">Sobre mim</h2>
             <p>
-              Olá, sou Washington Lemos. Sou apaixonado por tecnologia e
-              desenvolvimento de software, com uma forte dedicação em evoluir e
-              crescer na área de programação. Atualmente, estou cursando
-              Engenharia de Software e trabalhando como desenvolvedor front-end,
-              com experiência em projetos pessoais e conhecimento em tecnologias
-              como HTML, CSS, JavaScript, React e Next.js. Meu objetivo de
-              carreira é consolidar minha posição na área de tecnologia e
-              contribuir para projetos inovadores que façam a diferença. Estou
-              sempre em busca de novos desafios e oportunidades para aprender e
-              me aprimorar.
+              Olá, sou Washington, tenho 30 anos e sou natural de Recife.
+              Atualmente, moro entre Recife e São Paulo. Sou formado em Análise
+              e Desenvolvimento de Sistemas e estou cursando Engenharia de
+              Software para aprofundar meu conhecimento na área de tecnologia.
+            </p>
+            <p>
+              A programação foi uma verdadeira virada de chave na minha vida. Em
+              um momento em que estava em busca de crescimento pessoal e
+              profissional, a tecnologia me ofereceu uma oportunidade de
+              transformação e evolução. Decidi investir na programação não
+              apenas como uma carreira, mas como uma forma de desenvolver novas
+              habilidades e encontrar uma profissão que realmente me realizasse.
+              Essa jornada tem sido emocionante e desafiadora, e estou empolgado
+              com o futuro e as oportunidades que virão.
             </p>
             <span className={Styles.leftContainerArrow}></span>
           </div>
@@ -39,13 +43,17 @@ const About = () => {
         <div className={`${Styles.container} ${Styles.rightContainer}`}>
           <div className={Styles.iconContainer}></div>
           <div className={Styles.textBox}>
-            <h2 id="objetivos">Objetivos de Carreira</h2>
+            <h2 id="objetivos">Quando Tudo Começou</h2>
+            <h3>2021</h3>
             <p>
-              Minha meta é me destacar na área de desenvolvimento front-end,
-              contribuindo para projetos de impacto e alcançando reconhecimento
-              profissional. Busco oportunidades que me permitam aplicar meus
-              conhecimentos, aprender novas tecnologias e colaborar com equipes
-              criativas e inovadoras.
+              Em meados de 2021, decidi começar a estudar programação. Os
+              primeiros passos foram desafiadores; como em qualquer nova
+              jornada, houve momentos de dúvida e a sensação de que eu poderia
+              não conseguir aprender. No entanto, o meu interesse e paixão pela
+              tecnologia sempre foram maiores do que as dificuldades iniciais.
+              Esse período de aprendizado inicial foi fundamental para despertar
+              em mim a determinação e o entusiasmo necessários para seguir em
+              frente.
             </p>
             <span className={Styles.rightContainerArrow}></span>
           </div>
@@ -53,14 +61,23 @@ const About = () => {
         <div className={`${Styles.container} ${Styles.leftContainer}`}>
           <div className={Styles.iconContainer}></div>
           <div className={Styles.textBox}>
-            <h2 id="interesses">Áreas de Interesse na Programação</h2>
+            <h2 id="interesses">Desenvolvimento do Interesse em Front-End</h2>
+            <h3>2022</h3>
             <p>
-              Tenho interesse em desenvolvimento front-end e estou expandindo
-              meus conhecimentos em engenharia de software, desenvolvimento full
-              stack e banco de dados. Acredito que entender tanto o front-end
-              quanto o back-end é crucial para criar aplicações completas e
-              funcionais. Estou sempre em busca de novos desafios para aplicar e
-              aprimorar essas habilidades.
+              Após ter contato com HTML, CSS e JavaScript e me aprofundar no
+              desenvolvimento de páginas web estáticas e interativas, comecei a
+              me interessar mais pelo desenvolvimento front-end. A combinação
+              dessas tecnologias despertou minha paixão por criar interfaces
+              visuais e experiências de usuário envolventes. A possibilidade de
+              construir aplicações que não apenas funcionam bem, mas também são
+              agradáveis de usar, me motivou a explorar ainda mais essa área.
+            </p>
+            <p>
+              Comecei a estudar frameworks e bibliotecas como React e Material
+              UI, e me envolvi em projetos que exigiam uma maior atenção ao
+              design e à usabilidade. Essa nova paixão pelo front-end foi um
+              marco importante na minha jornada, direcionando minha carreira
+              para um campo que combina criatividade e técnica de forma única.
             </p>
             <span className={Styles.leftContainerArrow}></span>
           </div>
@@ -68,29 +85,82 @@ const About = () => {
         <div className={`${Styles.container} ${Styles.rightContainer}`}>
           <div className={Styles.iconContainer}></div>
           <div className={Styles.textBox}>
-            <h2 id="gostos">Gostos Pessoais</h2>
+            <h2 id="gostos">Progresso em Projetos Front-End e Formação</h2>
+            <h3>2023</h3>
             <p>
-              Sou uma pessoa apaixonada por viajar e explorar novos lugares.
-              Entre os diversos destinos que já tive a oportunidade de conhecer
-              pelo Brasil, a Chapada dos Veadeiros se destacou como o mais
-              marcante. Lá, tive a chance de me conectar profundamente com a
-              natureza através de suas cachoeiras deslumbrantes e trilhas
-              revitalizantes. A natureza exerce um efeito renovador em mim, e
-              sempre busco estar em contato com ela.
+              Em 2023, meu interesse por frameworks e bibliotecas front-end
+              continuou a crescer. Dediquei-me a criar diversos projetos
+              front-end, começando com meu primeiro portfólio, que foi
+              desenvolvido utilizando HTML, CSS e JavaScript. Esse projeto foi
+              uma oportunidade para aplicar o que aprendi e demonstrar minhas
+              habilidades na criação de interfaces e experiências de usuário.
+            </p>
+            <p>
+              Com o tempo, evoluí para um novo portfólio, desta vez utilizando
+              Next.js. Este novo projeto não só aprimorou minhas habilidades em
+              desenvolvimento front-end, mas também me permitiu explorar
+              recursos avançados e melhorar a funcionalidade e o desempenho das
+              minhas aplicações.
+            </p>
+            <p>
+              Além disso, em dezembro de 2023, concluí minha formação em Análise
+              e Desenvolvimento de Sistemas. Esse marco acadêmico foi um passo
+              importante na minha jornada, consolidando meu conhecimento e
+              preparando-me para desafios futuros na área de tecnologia.
+            </p>
+            <span className={Styles.rightContainerArrow}></span>
+          </div>
+        </div>
+        <div className={`${Styles.container} ${Styles.leftContainer}`}>
+          <div className={Styles.iconContainer}></div>
+          <div className={Styles.textBox}>
+            <h2 id="motivacoes">Avanços em </h2>
+            <h3>2024</h3>
+            <p>
+              Em 2024, comecei um novo curso de Engenharia de Software, buscando
+              aprofundar ainda mais meu conhecimento e habilidades na área de
+              tecnologia. Para refletir essa evolução, atualizei meu portfólio e
+              desenvolvi vários novos projetos.
+            </p>
+            <p>
+              Chegar até aqui foi uma jornada cheia de aprendizados e desafios,
+              e estou em busca de uma oportunidade de trabalho, seja um estágio
+              ou uma posição júnior. Acredito que posso evoluir ainda mais ao
+              vivenciar projetos reais e aplicar meus conhecimentos em um
+              ambiente profissional. Estou pronto para contribuir e crescer com
+              uma equipe dedicada e inovadora.
+            </p>
+            <span className={Styles.leftContainerArrow}></span>
+          </div>
+        </div>
+        <div className={`${Styles.container} ${Styles.rightContainer}`}>
+          <div className={Styles.iconContainer}></div>
+          <div className={Styles.textBox}>
+            <h2 id="gostos">
+              Se Você Chegou Até Aqui, Conheça um Pouco Mais Sobre Mim
+            </h2>
+            <p>
+              Além da programação, sou uma pessoa apaixonada por viajar e
+              explorar novos lugares. Entre os diversos destinos que já tive a
+              oportunidade de conhecer pelo Brasil, a Chapada dos Veadeiros se
+              destacou como o mais marcante. Lá, tive a chance de me conectar
+              profundamente com a natureza através de suas cachoeiras
+              deslumbrantes e trilhas revitalizantes. A natureza exerce um
+              efeito renovador em mim, e sempre busco estar em contato com ela.
             </p>
             <p>
               Além das viagens, tenho um grande apreço por esportes como futebol
               e ciclismo, que me ajudam a manter um estilo de vida ativo e
               saudável. No universo dos games, sou fã do estilo battlegrounds,
-              que sempre me proporciona momentos de diversão e desafio. Valorizo
-              muito o tempo com minha família e adoro aproveitar os fins de
-              semana para estar com eles, seja em atividades ao ar livre ou em
-              momentos de lazer.
+              que sempre me proporciona momentos de diversão e desafio.
             </p>
             <p>
-              Sou um grande apreciador de filmes e séries, e meus gêneros
-              preferidos são os de guerra e de heróis. As histórias intensas e
-              inspiradoras desses gêneros sempre me envolvem e me fascinam.
+              Valorizo muito o tempo com minha família e adoro aproveitar os
+              fins de semana para estar com eles, seja em atividades ao ar livre
+              ou em momentos de lazer. Sou também um grande apreciador de filmes
+              e séries, com uma preferência especial por aqueles com temas de
+              guerra e heróis. As histórias intensas e inspiradoras desses
+              gêneros sempre me envolvem e fascinam.
             </p>
             <span className={Styles.rightContainerArrow}></span>
           </div>
@@ -98,22 +168,67 @@ const About = () => {
         <div className={`${Styles.container} ${Styles.leftContainer}`}>
           <div className={Styles.iconContainer}></div>
           <div className={Styles.textBox}>
-            <h2 id="motivacoes">Motivações</h2>
+            <h2 id="motivacoes">Minhas Motivações e Visões de Futuro </h2>
             <p>
-              Minha principal motivação para participar da comunidade Código
-              Certo Coders é a oportunidade de crescer profissionalmente e me
-              conectar com outros desenvolvedores apaixonados por tecnologia.
-              Acredito que a comunidade oferece um ambiente enriquecedor para
-              compartilhar conhecimentos, aprender novas habilidades e colaborar
-              em projetos desafiadores. Quero me envolver ativamente, contribuir
-              com minhas habilidades e, ao mesmo tempo, absorver experiências e
-              insights de outros membros. Estou determinado a utilizar essa
-              plataforma para acelerar minha evolução na área de desenvolvimento
-              front-end, alcançar meus objetivos de carreira e mostrar meu
-              potencial em um espaço que valoriza a inovação e o aprendizado
-              contínuo.
+              Minha principal motivação é alcançar meu tão sonhado "sim",
+              provando para mim mesmo que sou capaz de crescer tanto
+              profissionalmente quanto pessoalmente. Este objetivo não é apenas
+              sobre conquistar uma posição ou um título, mas sobre demonstrar
+              minha capacidade de superar desafios e alcançar minhas metas com
+              dedicação e esforço.
+            </p>
+            <p>
+              Minha visão de futuro é viver fazendo o que aprendi a gostar,
+              especialmente no campo da tecnologia. Acredito que, com o
+              conhecimento e as habilidades que desenvolvi, poderei construir
+              uma carreira gratificante e significativa. Além disso, desejo
+              aproveitar as oportunidades que a tecnologia pode proporcionar
+              para viajar e conhecer novos lugares, experimentando momentos
+              únicos com minha família.
+            </p>
+            <p>
+              Com a ajuda da tecnologia, estou confiante de que poderei realizar
+              esses sonhos e criar uma vida plena e equilibrada. Acredito que a
+              combinação de crescimento profissional, exploração do mundo e
+              tempo de qualidade com meus entes queridos formará a base de um
+              futuro satisfatório e enriquecedor.
             </p>
             <span className={Styles.leftContainerArrow}></span>
+          </div>
+        </div>
+        <div className={`${Styles.container} ${Styles.rightContainer}`}>
+          <div className={Styles.iconContainer}></div>
+          <div className={Styles.textBox}>
+            <h2 id="gostos">O Que Esperar de Mim</h2>
+            <p>
+              Você pode esperar de mim uma grande vontade de aprender e uma
+              abordagem autodidata para o desenvolvimento profissional. Tenho
+              uma forte determinação para aprender na prática e enfrentar
+              desafios de frente, o que me permite crescer constantemente e
+              evoluir como profissional.
+            </p>
+            <p>
+              Uma das minhas principais qualidades é a capacidade de ser
+              autodidata. Estou sempre em busca de novas formas de adquirir
+              conhecimento e aprimorar minhas habilidades, mesmo fora dos
+              ambientes formais de aprendizado. Esse espírito proativo me ajuda
+              a enfrentar problemas complexos e encontrar soluções criativas.
+            </p>
+            <p>
+              Estou ciente de que o caminho não será fácil, mas acredito
+              firmemente que os desafios são oportunidades valiosas de
+              aprendizado. Estou preparado para encarar dificuldades, sabendo
+              que é através desses desafios que conseguiremos aprender e
+              evoluir.
+            </p>
+            <p>
+              Espero trazer para a equipe não apenas minhas habilidades
+              técnicas, mas também meu entusiasmo para enfrentar novos desafios
+              e minha disposição para crescer e me adaptar. Estou pronto para
+              contribuir e evoluir em um ambiente que valorize a inovação e o
+              desenvolvimento contínuo.
+            </p>
+            <span className={Styles.rightContainerArrow}></span>
           </div>
         </div>
       </div>
--- src/app/contact-page/page.js
+++ src/app/contact-page/page.js
@@ -1,51 +1,90 @@
-import { Box, Button, Typography } from '@mui/material';
-import Styles from './contact.module.css'
-import Link from 'next/link';
+import { Box, Button, Typography } from "@mui/material";
+import Styles from "./contact.module.css";
+import Link from "next/link";
 
 const ContactPage = () => {
-    return (
-        <section className={Styles.containerContact}>
-            <Box className={Styles.containerMenu}>
-                <Link href="/aboutMe-page" passHref>
-                    <Button className={Styles.button} variant="contained" >
-                        About me
-                    </Button>
-                </Link>
-                <Link href="/" passHref>
-                    <Button className={Styles.button} variant="contained" >
-                        Home
-                    </Button>
-                </Link>
-            </Box>
-            <Box className={Styles.container}>
-                <Box className={Styles.redes}>
-                    <Typography variant='h5' className={Styles.text}>
-                        Vamos Conversar?
-                    </Typography>
-                    <Box className={Styles.descricao}>
-                        <ul>
-                            <a href="https://www.linkedin.com/in/washington-lemos-033177186/" target="blank"><li>Linkedin</li></a>
-                            <a href="https://www.instagram.com/washington.lemos_/?hl=pt-br" target="blank"><li>Instagram</li></a>
-                            <a href="https://www.facebook.com/washington.lemos.961/?locale=pt_BR" target="blank"><li>Facebook</li></a>
-                            <a href="https://WA.me/5581982383803" target="blank"><li>Whatsapp</li></a>
-                        </ul>
-                    </Box>
-                </Box>
-                <Box className={Styles.formulario}>
-                    <Box className={Styles.form}>
-                        <form action="https://api.staticforms.xyz/submit" method="post">
-                            <input type="hidden" name="accessKey" value="71bf7d3e-399a-41d9-a329-ea4cb2e65419" />
-                            <input type="hidden" name="redirectTo" value="/" />
-                            <input type="text" name="name" placeholder="Seu Nome" required maxLength={25} minLength={2} />
-                            <input type="text" name="email" placeholder="Seu Email" required maxLength={50} />
-                            <textarea name="message" required maxLength={150} placeholder="Deixe sua mensagem"></textarea>
-                            <button>Enviar</button>
-                        </form>
-                    </Box>
-                </Box>
-            </Box>
-        </section>
-    );
+  return (
+    <section className={Styles.containerContact}>
+      <Box className={Styles.containerMenu}>
+        <Link href="/aboutMe-page" passHref>
+          <Button className={Styles.button} variant="contained">
+            About me
+          </Button>
+        </Link>
+        <Link href="/" passHref>
+          <Button className={Styles.button} variant="contained">
+            Home
+          </Button>
+        </Link>
+      </Box>
+      <Box className={Styles.container}>
+        <Box className={Styles.redes}>
+          <Typography variant="h5" className={Styles.text}>
+            Vamos Conversar?
+          </Typography>
+          <Box className={Styles.descricao}>
+            <ul>
+              <a
+                href="https://www.linkedin.com/in/washington-lemos-033177186/"
+                target="blank"
+              >
+                <li>Linkedin</li>
+              </a>
+              <a
+                href="https://www.instagram.com/washington.lemos_/?hl=pt-br"
+                target="blank"
+              >
+                <li>Instagram</li>
+              </a>
+              <a
+                href="https://www.facebook.com/washington.lemos.961/?locale=pt_BR"
+                target="blank"
+              >
+                <li>Facebook</li>
+              </a>
+              <a href="https://WA.me/5581982383803" target="blank">
+                <li>Whatsapp</li>
+              </a>
+            </ul>
+          </Box>
+        </Box>
+        <Box className={Styles.formulario}>
+          <Box className={Styles.form}>
+            <form action="https://api.staticforms.xyz/submit" method="post">
+              <input
+                type="hidden"
+                name="accessKey"
+                value="71bf7d3e-399a-41d9-a329-ea4cb2e65419"
+              />
+              <input type="hidden" name="redirectTo" value="/" />
+              <input
+                type="text"
+                name="name"
+                placeholder="Seu Nome"
+                required
+                maxLength={25}
+                minLength={2}
+              />
+              <input
+                type="text"
+                name="email"
+                placeholder="Seu Email"
+                required
+                maxLength={50}
+              />
+              <textarea
+                name="message"
+                required
+                maxLength={150}
+                placeholder="Deixe sua mensagem"
+              ></textarea>
+              <button>Enviar</button>
+            </form>
+          </Box>
+        </Box>
+      </Box>
+    </section>
+  );
 };
 
 export default ContactPage;
