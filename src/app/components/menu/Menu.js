7791c7ab9ddb48683b9999e222059745550f5d7f
Washington <washington.contato1327@gmail.com>
2024-08-29 19:33:11 -0300

    Alteração de menu

--- src/app/components/menu/Menu.js
+++ src/app/components/menu/Menu.js
@@ -1,28 +1,38 @@
-import Link from 'next/link';
-import Styles from './menu.module.css';
+import Link from "next/link";
+import Styles from "./menu.module.css";
 
 const Menu = () => {
-    return (
-        <nav className={Styles.nav}>
-            <ul className={Styles.ul}>
-                <li>
-                    <Link className={Styles.link} href="#home">Home</Link>
-                </li>
-                <li>
-                    <Link className={Styles.link} href="#projects">Projects</Link>
-                </li>
-                <li>
-                    <Link className={Styles.link} href="#skills">Skills</Link>
-                </li>
-                <li>
-                    <Link className={Styles.link} href="/aboutMe-page">About me</Link>
-                </li>
-                <li>
-                    <Link className={Styles.link} href="/contact-page">Contacts</Link>
-                </li>
-            </ul>
-        </nav>
-    );
+  return (
+    <nav className={Styles.nav}>
+      <ul className={Styles.ul}>
+        <li>
+          <Link className={Styles.link} href="#home">
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link className={Styles.link} href="#sobre">
+            About me
+          </Link>
+        </li>
+        <li>
+          <Link className={Styles.link} href="#projects">
+            Projects
+          </Link>
+        </li>
+        <li>
+          <Link className={Styles.link} href="#skills">
+            Skills
+          </Link>
+        </li>
+        <li>
+          <Link className={Styles.link} href="/contact-page">
+            Contacts
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Menu;
--- src/app/pages/index.js
+++ src/app/pages/index.js
@@ -1,23 +1,22 @@
-
-import React from 'react';
-import Projects from './projects/Projects';
-import Header from './header/header';
-import Home from './home/home';
-import Footer from './footer/footer';
-import Skills from './skills/skills';
+import React from "react";
+import Projects from "./projects/Projects";
+import Header from "./header/header";
+import Home from "./home/home";
+import Footer from "./footer/footer";
+import Skills from "./skills/skills";
+import About from "./About/page";
 
 const Index = () => {
-
-
-    return (
-        <>
-            <Header />
-            <Home />
-            <Projects />
-            <Skills />
-            <Footer />
-        </>
-    );
+  return (
+    <>
+      <Header />
+      <Home />
+      <About />
+      <Projects />
+      <Skills />
+      <Footer />
+    </>
+  );
 };
 
 export default Index;
