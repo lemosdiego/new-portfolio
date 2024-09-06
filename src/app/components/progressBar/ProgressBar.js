2c06cdf81418fa9966fe9c6f0ff14e24298aec60
Washington <washington.contato1327@gmail.com>
2024-09-05 21:07:44 -0300

    Adicionando funcionalidade na barra de progresso

--- src/app/components/progressBar/ProgressBar.js
+++ src/app/components/progressBar/ProgressBar.js
@@ -1,18 +1,37 @@
-import { Box, Typography } from '@mui/material'
-import Styles from './progressBar.module.css'
+"use client";
+
+import { useEffect, useRef, useState } from "react";
+import { Box, Typography } from "@mui/material";
+import Styles from "./progressBar.module.css";
+import { useInView } from "react-intersection-observer";
 
 const ProgressBar = ({ title, percentage, color }) => {
-    return (
-        <Box className={Styles.container}>
-            <Box className={Styles.containerText}>
-                <Typography>{title}</Typography>
-                <Typography>{percentage}</Typography>
-            </Box>
-            <Box className={Styles.containerProgress}>
-                <Box className={Styles.progress} sx={{ width: `${percentage}`, backgroundColor: `${color}` }}></Box>
-            </Box>
-        </Box>
-    )
-}
+  const [progressWidth, setProgressWidth] = useState("0%");
+  const { ref, inView } = useInView({
+    triggerOnce: true, // only trigger the animation once
+    threshold: 0.1, // trigger when 10% of the element is visible
+  });
 
-export default ProgressBar
+  useEffect(() => {
+    if (inView) {
+      setProgressWidth(percentage);
+    }
+  }, [inView, percentage]);
+
+  return (
+    <Box className={Styles.container}>
+      <Box className={Styles.containerText}>
+        <Typography>{title}</Typography>
+        <Typography>{percentage}</Typography>
+      </Box>
+      <Box className={Styles.containerProgress} ref={ref}>
+        <Box
+          className={Styles.progress}
+          sx={{ width: progressWidth, backgroundColor: color }}
+        />
+      </Box>
+    </Box>
+  );
+};
+
+export default ProgressBar;
