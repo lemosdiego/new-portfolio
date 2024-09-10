2c9cf3fe8c7977155b8f6c2baaa7b75a1da379e6
Washington <washington.contato1327@gmail.com>
2024-09-10 08:51:42 -0300

    Subistituição de componene de button em LanguageSelector

--- src/app/components/LanguageSelector/LanguageSelector.js
+++ src/app/components/LanguageSelector/LanguageSelector.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useLanguage } from "../../../../context/LanguageContext";
 import Styles from "./LanguageSelector.module.css";
-import { Box, Typography } from "@mui/material";
+import { Box, Button } from "@mui/material";
 
 const LanguageSelector = () => {
   const { changeLanguage } = useLanguage();
@@ -13,28 +13,19 @@ const LanguageSelector = () => {
   return (
     <Box className={Styles.languageSelector}>
       <Box className={`${Styles.flag} ${Styles.br}`}>
-        <Typography
-          className={Styles.p}
-          onClick={() => handleLanguageChange("pt")}
-        >
+        <Button className={Styles.p} onClick={() => handleLanguageChange("pt")}>
           PT
-        </Typography>
+        </Button>
       </Box>
       <Box className={`${Styles.flag} ${Styles.en}`}>
-        <Typography
-          className={Styles.p}
-          onClick={() => handleLanguageChange("en")}
-        >
+        <Button className={Styles.p} onClick={() => handleLanguageChange("en")}>
           EN
-        </Typography>
+        </Button>
       </Box>
       <Box className={`${Styles.flag} ${Styles.es}`}>
-        <Typography
-          className={Styles.p}
-          onClick={() => handleLanguageChange("es")}
-        >
+        <Button className={Styles.p} onClick={() => handleLanguageChange("es")}>
           ES
-        </Typography>
+        </Button>
       </Box>
     </Box>
   );
